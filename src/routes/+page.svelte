fc10e0369457da809943e15fe79c0b6cd143ed73
dante01yoon <bunggl@naver.com>
2023-07-05 20:30:31 +0900

    feat: add await block example page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	const links = ['logic-key'];
+	const links = ['logic-key', 'logic-await'];
 </script>
 
 <title>svelte tutorial</title>
---
+++ src/routes/logic-await/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { getRandomNumber } from '~/utils';
+
+	let promise = getRandomNumber();
+	let promiseLong = getRandomNumber(5000);
+</script>
+
+<h1>Await blocks example</h1>
+<div>
+	<h2>pending 3s</h2>
+	{#await promise}
+		<p>...waiting</p>
+	{:then number}
+		<p>The number is {number}</p>
+	{:catch error}
+		<p style="color: red;">{error.message}</p>
+	{/await}
+</div>
+<div>
+	<h2>pending 5s without '...waiting' ui</h2>
+	{#await promiseLong then number}
+		<p>The number is {number}</p>
+	{/await}
+</div>
